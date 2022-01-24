adeb644715f848da951621c3fc81514a7d01a75b
SamuelLuzSantana <slsamuelluz@gmail.com>
2022-01-24 12:07:39 -0300

    inicio do curso de javascript

---
+++ JavaScript ES6/Fundamentos_JavaScript/variaveis/script.js
@@ -0,0 +1,21 @@
+
+//Patrice
+
+//Declara uma variavel com seu nome
+var nome = "Samuel";
+
+//Declarar uma variavel com a sua idade
+var idade = 18;
+
+//Declarar uma variavel com a sua comida preferida e não atribuir um valor
+var comidaPreferida;
+
+//Atribuir valor a sua comida preferida
+comidaPreferida = "Batata Frita";
+
+//Declarar 5 variaveis sem valor
+var cachorro;
+var fruta;
+var carro;
+var linguagem;
+var pais;
