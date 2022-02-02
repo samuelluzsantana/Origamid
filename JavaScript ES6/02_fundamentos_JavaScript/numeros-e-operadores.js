f6ea1c201969bf65f584151328780a3e4d056692
SamuelLuzSantana <slsamuelluz@gmail.com>
2022-02-02 11:06:41 -0300

    Bolean e condicionais

---
+++ JavaScript ES6/02_fundamentos_JavaScript/numeros-e-operadores.js
@@ -0,0 +1,37 @@
+
+var idade = 28;
+var gols = 1000;
+var pi = 3.14; // ponto para decimal
+var exp = 2e10; // 20000000000
+
+
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+
+
+var soma = '100' + 50; // 10050
+var subtracao = '100' - 50; // 50
+var multiplicacao = '100' * '2'; // 200
+var divisao = 'Comprei 10' / 2; // NaN (Not a Number)
+
+//Exercício
+
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+
+// Crie duas expressões que retornem NaN
+
+// Somar a string '200' com o número 50 e retornar 250
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2; // NaN (Not a Number)
