a5745a2a839e91c37e3063004a76b46c70d94494
Samuel Luz Santana <samuelluzsantana@outlook.com>
2023-04-24 10:32:58 -0300

    exercício Union Types

--- TypeScript/0203/script.ts
+++ TypeScript/0203/script.ts
@@ -1,2 +1,19 @@
 //0203 Union Types 1
 
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+
+function toNumber(value: string | number) {
+    if (typeof value === 'string') {
+        return Number(value);
+    } else if (typeof value === 'number') {
+        return value;
+    } else {
+        throw 'value deve ser um número ou uma string';
+    }
+}
+
+console.log(toNumber('asda'));
