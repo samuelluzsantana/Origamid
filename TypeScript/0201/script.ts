124c3bf2ee0aafbe007f64809c20d3d74267e28d
Samuel Luz Santana <samuelluzsantana@outlook.com>
2023-04-24 09:31:27 -0300

    Resolução dos exercícios 201

--- TypeScript/0201/script.ts
+++ TypeScript/0201/script.ts
@@ -1,7 +1,29 @@
+const input = document.querySelector('input');
 
+const total = localStorage.getItem('total');
 
-function normalizarTexto(texto: string) {
-    return texto.trim().toLowerCase();
+if (input && total) {
+  input.value = total;
+  calcularGanho(Number(input.value));
 }
 
-console.log(normalizarTexto('HELPEME,adADADDDAD SOCORRO'));
+
+function calcularGanho(value: number) {
+  const p = document.querySelector('p');
+  if (p) {
+    p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+  }
+}
+
+function totalMudou() {
+  if (input) {
+    localStorage.setItem('total',input.value);
+    calcularGanho(Number(input.value));
+  }
+}
+
+
+if (input) {
+  input.addEventListener('keyup', totalMudou);
+}
+
