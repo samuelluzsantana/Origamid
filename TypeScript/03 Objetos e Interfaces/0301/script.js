9f0c6ad5739192f9a2615e731da39880f48f2047
Samuel Luz Santana <samuelluzsantana@outlook.com>
2023-04-26 10:57:48 -0300

    0301 - Instanceof

---
+++ TypeScript/03 Objetos e Interfaces/0301/script.js
@@ -0,0 +1,6 @@
+"use strict";
+//0301 - Instanceof 
+const link = document.getElementById('origamid');
+if (link instanceof HTMLAnchorElement) {
+    link.href = link.href.replace('http://', 'https://');
+}
